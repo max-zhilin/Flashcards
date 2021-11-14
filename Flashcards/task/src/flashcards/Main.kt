4c8cc5f0514064ac8b9fa2b0c9aff25f13a07bab
Max Zhilin <max_zhilin@mail.ru>
2021-11-14 17:36:36 +0300

    MenuPlease final

--- Flashcards/task/src/flashcards/Main.kt
+++ Flashcards/task/src/flashcards/Main.kt
@@ -1,63 +1,121 @@
 package flashcards
 
+import java.io.File
+import java.util.Properties
+
 fun main() {
-    println("Input the number of cards:")
-    val n = readLine()!!.toInt()
+    val cards = Cards()
 
-    val cards = Cards(n) { index ->
-        println("Card #${index + 1}:")
-        val term = input("term")
+    do {
+        println("Input the action (add, remove, import, export, ask, exit):")
+        val action = readLine()!!
+        when (action) {
+            "add" -> {
+                println("The card:")
+                val term = cards.inputOrElse("term") { entered ->
+                    println("The card \"$entered\" already exists.")
+                    null
+                } ?: continue
 
-        println("The definition for card #${index + 1}:")
-        val definition = input("definition")
+                println("The definition of the card:")
+                val definition = cards.inputOrElse("definition") { entered ->
+                    println("The definition \"$entered\" already exists.")
+                    null
+                } ?: continue
 
-        Card(term, definition)  // return to init Cards(n)
-    }
-
-    for (card in cards) {
-        println("Print the definition of \"${card.term}\":")
-        val answer = readLine()!!
-        if (answer == card.definition)
-            println("Correct!")
-        else with (cards.lookUpBy("definition", answer)) {
-            when(this) {
-                is NotFound -> println("Wrong. The right answer is \"${card.definition}\".")
-                is Found ->
-                    println("Wrong. The right answer is \"${card.definition}\", but your definition is correct for \"${this.card.term}\".")
+                cards.add(term, definition)
+                println("The pair (\"$term\":\"$definition\") has been added.")
+            }
+            "remove" -> {
+                println("Which card?")
+                val term = readLine()!!
+                if (cards.remove(term)) println("The card has been removed.")
+                else println("Can't remove \"$term\": there is no such card.")
+            }
+            "import" -> {
+                println("File name:")
+                val fileName = readLine()!!
+                val n = cards.import(fileName)
+                if (n == null) println("File not found.")
+                else println("$n cards have been loaded.")
+            }
+            "export" -> {
+                println("File name:")
+                val fileName = readLine()!!
+                val n = cards.export(fileName)
+                println("$n cards have been saved.")
+            }
+            "ask" -> {
+                println("How many times to ask?")
+                val n = readLine()!!.toInt()
+                repeat(n) {
+                    val (term, definition) = cards.getRandomCard()
+                    println("Print the definition of \"$term\":")
+                    val answer = readLine()!!
+                    if (answer == definition)
+                        println("Correct!")
+                    else with(cards.lookUpOrNull("definition", answer)) {
+                        when (this) {
+                            null -> println("Wrong. The right answer is \"$definition\".")
+                            else -> println("Wrong. The right answer is \"$definition\", but your definition is correct for \"$this\".")
+                        }
+                    }
+                }
             }
         }
+    } while (action != "exit")
+
+    println("Bye bye!")
+}
+
+class Cards {
+    private val cardsMap = mutableMapOf<String, String>()
+
+    fun inputOrElse(part: String, whenExists: (String) -> String?): String? {
+        val text = readLine()!!
+        return if (lookUpOrNull(part, text) == null) text
+        else whenExists(text)
+    }
+
+    fun lookUpOrNull(part: String, text: String): String? {
+        return if (part == "term")
+            cardsMap[text]
+        else
+            cardsMap.getKeyByValue(text)
+    }
+
+    fun add(term: String, definition: String) {
+        cardsMap[term] = definition
+    }
+
+    fun remove(term: String): Boolean = cardsMap.remove(term) != null
+
+    fun getRandomCard(): Pair<String, String> = cardsMap.entries.random().toPair()
+
+    fun import(fileName: String): Int? {
+        val props = Properties()
+        val file = File(fileName)
+        try {
+            props.load(file.inputStream())
+        } catch (e: Exception) {
+            return null
+        }
+        props.forEach {
+            cardsMap[it.key as String] = it.value as String
+        }
+        return props.size
+    }
+
+    fun export(fileName: String): Int {
+        val props = Properties()
+        props.putAll(cardsMap)
+        val file = File(fileName)
+        props.store(file.outputStream(), null)
+        return cardsMap.size
     }
 }
 
-class Cards(size: Int, init: Cards.(index: Int) -> Card) : Iterable<Card> {
-    private val cardsList = mutableListOf<Card>()
-
-    init {
-        repeat(size) { index -> cardsList.add(init(index)) }
-    }
-    override fun iterator(): Iterator<Card> = cardsList.iterator()
-
-    fun input(part: String): String {
-        var text = readLine()!!
-        do {
-            val searchResult = lookUpBy(part, text)
-            if (searchResult is Found) {
-                println("The $part \"$text\" already exists. Try again:")
-                text = readLine()!!
-            }
-        } while (searchResult is Found)
-        return text
-    }
-
-    fun lookUpBy(part: String, text: String): SearchResult {
-        val cardOrNull = cardsList.find { text == if (part == "term") it.term else it.definition }
-
-        return if (cardOrNull == null) NotFound else Found(cardOrNull)
-    }
+fun <K, V> Map<out K, V>.getKeyByValue(value: V): K? {
+    for (element in this) if (element.value == value) return element.key
+    return null
 }
-
-sealed class SearchResult
-object NotFound : SearchResult()
-class Found(val card: Card) : SearchResult()
-
-data class Card(val term: String, val definition: String)
