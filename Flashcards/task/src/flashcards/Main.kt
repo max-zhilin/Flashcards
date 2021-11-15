0101237aedc7d175dcaede5e6ccba02c476805fc
Max Zhilin <max_zhilin@mail.ru>
2021-11-15 12:09:46 +0300

    Statistics done

--- Flashcards/task/src/flashcards/Main.kt
+++ Flashcards/task/src/flashcards/Main.kt
@@ -6,27 +6,33 @@
 package flashcards
 
 import java.io.File
-import java.util.Properties
+import java.io.IOException
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+import java.io.Serializable
+
 
 fun main() {
     val cards = Cards()
 
     do {
-        println("Input the action (add, remove, import, export, ask, exit):")
+        println("Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):")
         val action = readLine()!!
         when (action) {
             "add" -> {
                 println("The card:")
-                val term = cards.inputOrElse("term") { entered ->
-                    println("The card \"$entered\" already exists.")
-                    null
-                } ?: continue
+                val term = readLine()!!
+                if (cards.contains("term", term)) {
+                    println("The card \"$term\" already exists.")
+                    continue
+                }
 
                 println("The definition of the card:")
-                val definition = cards.inputOrElse("definition") { entered ->
-                    println("The definition \"$entered\" already exists.")
-                    null
-                } ?: continue
+                val definition = readLine()!!
+                if (cards.contains("definition", definition)) {
+                    println("The definition \"$definition\" already exists.")
+                    continue
+                }
 
                 cards.add(term, definition)
                 println("The pair (\"$term\":\"$definition\") has been added.")
@@ -54,18 +60,43 @@ fun main() {
                 println("How many times to ask?")
                 val n = readLine()!!.toInt()
                 repeat(n) {
-                    val (term, definition) = cards.getRandomCard()
+                    val (term, card) = cards.getRandomCard()
                     println("Print the definition of \"$term\":")
                     val answer = readLine()!!
-                    if (answer == definition)
+                    if (answer == card.definition)
                         println("Correct!")
                     else with(cards.lookUpOrNull("definition", answer)) {
                         when (this) {
-                            null -> println("Wrong. The right answer is \"$definition\".")
-                            else -> println("Wrong. The right answer is \"$definition\", but your definition is correct for \"$this\".")
+                            null -> println("Wrong. The right answer is \"${card.definition}\".")
+                            else -> println("Wrong. The right answer is \"${card.definition}\", but your definition is correct for \"$this\".")
                         }
+                        card.mistakes++
                     }
                 }
+            }
+            "log" -> {
+                println("File name:")
+                val fileName = readLine()!!
+                Log.save(fileName)
+                println("The log has been saved.")
+            }
+            "hardest card" -> {
+                val hardestCardsMap = cards.getHardestCards()
+                when (hardestCardsMap.size) {
+                    0 -> println("There are no cards with errors.")
+                    1 -> with (hardestCardsMap.entries.first()) {
+                        println("The hardest card is \"$key\". You have ${value.mistakes} errors answering it.")
+                    }
+                    else -> {
+                        val terms = hardestCardsMap.map { it.key }.joinToString(", ") { "\"$it\"" }
+                        val mistakes = hardestCardsMap.entries.first().value.mistakes
+                        println("The hardest cards are $terms. You have $mistakes errors answering them.")
+                    }
+                }
+            }
+            "reset stats" -> {
+                cards.resetStats()
+                println("Card statistics have been reset.")
             }
         }
     } while (action != "exit")
@@ -74,53 +105,81 @@ fun main() {
 }
 
 class Cards {
-    private val cardsMap = mutableMapOf<String, String>()
+    private val cardsMap = mutableMapOf<String, Card>()
 
-    fun inputOrElse(part: String, whenExists: (String) -> String?): String? {
-        val text = readLine()!!
-        return if (lookUpOrNull(part, text) == null) text
-        else whenExists(text)
-    }
+    fun contains(part: String, text: String): Boolean = lookUpOrNull(part, text) != null
 
     fun lookUpOrNull(part: String, text: String): String? {
         return if (part == "term")
-            cardsMap[text]
+            cardsMap[text]?.definition
         else
-            cardsMap.getKeyByValue(text)
+            cardsMap.getKeyByDefinition(text)
     }
 
     fun add(term: String, definition: String) {
-        cardsMap[term] = definition
+        cardsMap[term] = Card(definition)
     }
 
     fun remove(term: String): Boolean = cardsMap.remove(term) != null
 
-    fun getRandomCard(): Pair<String, String> = cardsMap.entries.random().toPair()
+    fun getRandomCard(): Pair<String, Card> = cardsMap.entries.random().toPair()
 
     fun import(fileName: String): Int? {
-        val props = Properties()
-        val file = File(fileName)
         try {
-            props.load(file.inputStream())
-        } catch (e: Exception) {
+            File(fileName).inputStream().use {
+                val ois = ObjectInputStream(it)
+                val hashMap = ois.readObject() as HashMap<String, Card>
+                ois.close()
+                cardsMap.putAll(hashMap)
+                return hashMap.size
+            }
+        } catch (e: IOException) {
             return null
         }
-        props.forEach {
-            cardsMap[it.key as String] = it.value as String
-        }
-        return props.size
     }
 
     fun export(fileName: String): Int {
-        val props = Properties()
-        props.putAll(cardsMap)
-        val file = File(fileName)
-        props.store(file.outputStream(), null)
+        File(fileName).outputStream().use {
+            val oos = ObjectOutputStream(it)
+            val hashMap = HashMap(cardsMap)
+            oos.writeObject(hashMap)
+            oos.close()
+        }
         return cardsMap.size
+    }
+
+    fun getHardestCards(): Map<String, Card> {
+        val list = cardsMap.map { it.value.mistakes }
+        val max = list.maxOrNull()
+        return cardsMap.filterValues { it.mistakes == max && it.mistakes > 0}
+    }
+
+    fun resetStats() {
+        cardsMap.forEach { it.value.mistakes = 0 }
     }
 }
 
-fun <K, V> Map<out K, V>.getKeyByValue(value: V): K? {
-    for (element in this) if (element.value == value) return element.key
+data class Card(val definition: String, var mistakes: Int = 0) : Serializable
+
+fun Map<String, Card>.getKeyByDefinition(definition: String): String? {
+    for (element in this) if (element.value.definition == definition) return element.key
     return null
+}
+
+fun println(str: String) {
+    kotlin.io.println(str)
+    Log.add(str)
+}
+
+fun readLine(): String? {
+    val str = kotlin.io.readLine()
+    str?.let { Log.add(it) }
+    return str
+}
+
+object Log : ArrayList<String>() {
+
+    fun save(fileName: String) {
+        File(fileName).writeText(this.joinToString("\n"))
+    }
 }
